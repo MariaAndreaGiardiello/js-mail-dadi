b64eb5386cc2a4c3a6c70313828ec7e42e037f4c
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-03 19:03:05 +0200

    inizio esericio + struttura problema

---
+++ Mail/js/main.js
@@ -0,0 +1,14 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// 1. Creare costante con messaggio per inserimento e-mail utente
+const userMail = prompt("Inserisci la tua e-mail");
+console.log(userMail);
+
+// 2. Creare array che contenga le e-mail concesse
+const rightMail = ['giuseppe.verdi@gmail.com', 'giuseppe.verdi@icloud.com', 'giuseppe.verdi@libero.com', 'giuseppe.verdi@outlook.com'];
+
+// 3. Creare ciclo per convalida email utente
+// 4. Creare alert per email non valida
+// 5. Creare messaggio per conferma convalidazione mail e accesso al sito.

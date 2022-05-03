81ab2ff9653987e486b1425e67fee65b2d438281
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-03 21:43:51 +0200

    aggiunto risoluzione completa per estrazione (javascript)

--- Dadi/js/main.js
+++ Dadi/js/main.js
@@ -2,7 +2,22 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
-// 1. creo costante per contenere numeri da 1-6
-// 2. Creo variabile per estrazione utente/computer
-// 3. Creo ciclo per estrazione numeri
+// 1. creo costante per contenere numeri da 1-6 uente
+const randomNumberUser = Math.floor(Math.random() * 6) + 1;
+// 2. Creo variabile per estrazione utente
+let user = randomNumberUser;
+console.log(user);
+// 3. creo costante per contenere numeri da 1-6 uente
+const randomNumberComp = Math.floor(Math.random() * 6) + 1;
+// 4. Creo variabile per estrazione utente
+let computer = randomNumberComp;
+console.log(computer);
+
 // 4. Creo ciclo per stabilire il vincitore.
+if (user > computer) {
+    alert("Complimenti user hai vinto!!")
+} else if (user === computer) {
+    alert("Stesso numero, riprova!!")
+} else if (user < computer) {
+    alert("Complimenti computer hai vinto!!")
+}

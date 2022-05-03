4b2995518434a9fa5267617fe1b3ad443e3301e0
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-03 21:34:34 +0200

    struttura iniziale + analisi problema

---
+++ Dadi/js/main.js
@@ -0,0 +1,8 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// 1. creo costante per contenere numeri da 1-6
+// 2. Creo variabile per estrazione utente/computer
+// 3. Creo ciclo per estrazione numeri
+// 4. Creo ciclo per stabilire il vincitore.

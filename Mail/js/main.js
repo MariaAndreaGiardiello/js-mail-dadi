27e20166b1d71b42762275e2f1812a59cd33850a
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-03 20:43:19 +0200

    corretto ciclo convalida e-mail

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -2,13 +2,23 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-// 1. Creare costante con messaggio per inserimento e-mail utente
-const userMail = prompt("Inserisci la tua e-mail");
-console.log(userMail);
+// 1. Creare prompt per inserimento e-mail utente
+const usermail = prompt("Inserisci la tua mail");
 
-// 2. Creare array che contenga le e-mail concesse
-const rightMail = ['giuseppe.verdi@gmail.com', 'giuseppe.verdi@icloud.com', 'giuseppe.verdi@libero.com', 'giuseppe.verdi@outlook.com'];
+// 2. Creare array che contenga le email concesse
+const rightMail = ["giuseppe.verdi@gmail.com", "giuseppe.verdi@icloud.com", "giuseppe.verdi@libero.com", "giuseppe.verdi@outlook.com"];
+console.log(rightMail);
 
 // 3. Creare ciclo per convalida email utente
-// 4. Creare alert per email non valida
-// 5. Creare messaggio per conferma convalidazione mail e accesso al sito.
+for (let i = 0; i < rightMail.length; i++) {
+    if (usermail === rightMail[i]) {
+        console.log(rightMail[i]);
+// 4. Creare alert per verifica andata a buon fine
+        alert("Complimenti, e-mail verificata correttamente. Buona navigazione!")
+    } else {
+        console.log(rightMail[i]);
+// 5. Creare alert per verifica non andata a buon fine
+        alert("Si prega di inserire una nuova e-mail; formato non supportato")
+    }
+    break
+}
